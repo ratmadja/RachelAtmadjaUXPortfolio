2792afd919da68434c09e55947fa7003edaaba0f
ratmadja <ratmadja@gmail.com>
2018-07-12 13:08:17 -0700

    first commit

---
+++ scripts/master.js
@@ -0,0 +1,8 @@
+// Start of Syntax to make an entire div clickable
+
+// $(".work-NimbusDashCover").click (function () {
+//   window.location = $(this).find("a").attr("href");
+//   return false;
+// });
+
+// End of Syntax to make an entire div clickable
