789bdbc6ba1211b1ed4fdb1afc8345bef098d1fc
ratmadja <ratmadja@gmail.com>
2018-07-19 17:34:09 -0700

    updated stickyNav jQuery

--- scripts/master.js
+++ scripts/master.js
@@ -1,35 +1,54 @@
-//Start of 'JQuery scroll to element on page'
-//Code requires that the href attribute of <a> matches the id attribute of said element
+function onReady() {
 
-$('a[href*=#]:not([href=#])').click(function() {
+
+  //JQuery for Jump-to-element
+  //Will only work if href of <a> matches id of element to jump to
+
+  $('a[href*=#]:not([href=#])').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
-        || location.hostname == this.hostname) {
+    || location.hostname == this.hostname) {
 
-        var target = $(this.hash);
-        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-           if (target.length) {
-             $('html,body').animate({
-                 scrollTop: target.offset().top -100
-            }, 1000);
-            return false;
-        }
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top -100
+        }, 1000);
+        return false;
+      }
     }
-});
-
-//End of 'JQuery scroll to element on page'
+  });
 
 
+  
+  //JQuery for stickyTopNav
 
-//Start of 'JQuery sticky nav'
-//Code requires creation of .sticky class in css
-//Control the padding of sticky nav by media-query
+  const stickyTopNav = $('#navmenu').offset().top;
 
-$(window).scroll(function() {
-  if($(this).scrollTop()>650) {
-    $('.navmenu').addClass("sticky");
-  } else {
-    $('.navmenu').removeClass("sticky");
-  }
-});
+  let stickyNav = function() {
+    const scrollTop = $(window).scrollTop();
+    $('#navmenu').toggleClass(
+      'sticky',
+      scrollTop > stickyTopNav
+    );
+  };
 
-//End of 'JQuery sticky nav'
+  $(window).scroll(function() {
+      stickyNav();
+  });
+
+};
+
+
+window.onload = function() {
+  onReady();
+};
+
+
+// $(window).scroll(function() {
+//   if($(this).scrollTop()>650) {
+//     $('.navmenu').addClass("sticky");
+//   } else {
+//     $('.navmenu').removeClass("sticky");
+//   }
+// });
